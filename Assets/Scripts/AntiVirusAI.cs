92dc144a32363c25b01f6d5b973a2bd8d77c874b
TanaTanoi <tana.tanoi@powershop.co.nz>
2016-08-27 23:01:44 +1200

    Improved Antivirus AI to not fall
    AV don't fall off the edge of platforms now, but will move along all kinds of platforms
    Added more music
    Fixed minor bugs with some prefabs

--- Assets/Scripts/AntiVirusAI.cs
+++ Assets/Scripts/AntiVirusAI.cs
@@ -7,14 +7,25 @@ public class AntiVirusAI : MonoBehaviour {
 	// 1 for right, -1 for left
 	public int direction = 1;
 	public int speed = 3;
+
+	private float ground_scale;
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D> ();
 		render = GetComponent<SpriteRenderer> ();
+		ground_scale = transform.localScale.x * 1f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (TouchingGroundLeft () != TouchingGroundRight()) {
+			rb.velocity = Vector2.zero;
+			if (TouchingGroundLeft ()) {
+				rb.AddForce (new Vector2 (-5, 0));
+			} else {
+				rb.AddForce (new Vector2 (5, 0));
+			}
+		}
 		if (Mathf.Abs(rb.velocity.x) < 0.01f) {
 			direction *= -1;
 		}
@@ -40,5 +51,39 @@ public class AntiVirusAI : MonoBehaviour {
 	void TriggeredByExploit(){
 		render.color = Color.black;
 		speed = 8;
+	}
+
+	bool TouchingGroundLeft(){
+		RaycastHit2D x = Physics2D.Raycast(transform.position, LeftDirectionVector(), 3f, LayerMask.GetMask("Ground"));
+		if (x) {
+			print (x.collider.name);
+		} else {
+			print ("noneL");
+		}
+		return x;
+	}
+
+	bool TouchingGroundRight(){
+		RaycastHit2D x = Physics2D.Raycast(transform.position, RightDirectionVector(), 3f, LayerMask.GetMask("Ground"));
+		if (x) {
+			print (x.collider.name);
+		} else {
+			print ("noneR");
+		}
+		return x;
+	}
+	Vector2 LeftDirectionVector(){
+		Vector2 lower_left = rb.position + new Vector2 (-0.5f, -0.5f);
+		print ("L");
+		print (lower_left);
+		return (lower_left - rb.position).normalized;
+
+
+	}
+
+	Vector2 RightDirectionVector(){
+		Vector2 lower_right = rb.position + new Vector2 (0.5f, -0.5f);
+		return (lower_right - rb.position);
+
 	}
 }
