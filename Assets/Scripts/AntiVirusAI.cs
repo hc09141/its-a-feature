2e42827a2ad8d3783553294a9f077b3d1589ded3
TanaTanoi <tana.tanoi@powershop.co.nz>
2016-08-28 15:19:57 +1200

    Added bugs for exploit world and fixed scripts
    :

--- Assets/Scripts/AntiVirusAI.cs
+++ Assets/Scripts/AntiVirusAI.cs
@@ -23,27 +23,29 @@ public class AntiVirusAI : MonoBehaviour {
 			rb.velocity = Vector2.zero;
 			if (TouchingGroundLeft ()) {
 				rb.AddForce (new Vector2 (-5, 0));
+				FlipLeft ();
 			} else {
 				rb.AddForce (new Vector2 (5, 0));
+				FlipRight ();
 			}
 		}
 		if (Mathf.Abs(rb.velocity.x) < 0.01f) {
 			direction *= -1;
+			if (direction >= 0) {
+				FlipRight ();
+			} else {
+				FlipLeft ();
+			}
+				
 		}
 
 		Color n_c = nice.color;
 		Color e_c = evil.color;
-//		n_c.r = Mathf.Min (1f, n_c.r + 0.01f);
-//		n_c.b = Mathf.Min (1f, n_c.b + 0.01f);
-//		n_c.g = Mathf.Min (1f, n_c.g + 0.01f);
-//		e_c.r = 1.0f - n_c.r;
-//		e_c.b = 1.0f - n_c.b;
-//		e_c.g = 1.0f - n_c.g;
 		n_c.a = Mathf.Min(1.0f, n_c.a + 0.01f);
 		e_c.a = 1.0f - n_c.a;
 		nice.color = n_c;
 		evil.color = e_c;
-		if (n_c.r == 1.0f) {
+		if (n_c.a == 1.0f) {
 			speed = 3;
 		}
 
@@ -51,9 +53,19 @@ public class AntiVirusAI : MonoBehaviour {
 	}
 
 	void OnCollisionEnter2D(Collision2D collision){
-		if (collision.gameObject.tag == "Player" && speed == 8) {
+		if (collision.gameObject.tag == "Player" && speed > 3) {
 			SceneManager.LoadScene (SceneManager.GetActiveScene ().name);
 		}
+	}
+
+	void FlipLeft(){
+		evil.flipX = true;
+		nice.flipX = true;
+	}
+
+	void FlipRight(){
+		evil.flipX = false;
+		nice.flipX = false;
 	}
 
 	void TriggeredByExploit(){
