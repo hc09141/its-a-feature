bb0b701d84fd6c4a5506e0011e3d935cc509ba31
TanaTanoi <tana.tanoi@powershop.co.nz>
2016-08-28 17:08:51 +1200

    Platforms last longer

--- Assets/Scripts/HiddenPlatformScript.cs
+++ Assets/Scripts/HiddenPlatformScript.cs
@@ -14,7 +14,8 @@ public class HiddenPlatformScript : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		float alpha = Mathf.Max (0f, render.color.a - 0.05f);
+		float decrease = (1.0f - render.color.a) / 20.0f;
+		float alpha = Mathf.Max (0f, render.color.a - decrease);
 		Color c = render.color;
 		c.a = alpha;
 		render.color = c;
@@ -22,7 +23,7 @@ public class HiddenPlatformScript : MonoBehaviour {
 
 	void TriggeredByExploit(){
 		Color c = render.color;
-		c.a = 1.0f;
+		c.a = 0.99f;
 		render.color = c;
 	}
 }
